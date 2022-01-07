ebcddcdc78ae8511a65f5302375864c7b20d9d5a
Marcos <meunik@live.com>
2022-01-07 18:45:18 -0300

    api

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/', [UberController::class, 'index']);
 Route::post('/uber/salvar', [UberController::class, 'salvar']);
 Route::get('/uber/buscar/{data}', [UberController::class, 'buscar']);
