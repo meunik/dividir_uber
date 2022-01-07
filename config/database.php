e3511f547bc88e031d1644f0ac97138929289c48
Marcos <meunik@live.com>
2022-01-07 18:29:19 -0300

    banco heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = isset($url["host"]) ? $url["host"] : env('DB_HOST');
+$username = isset($url["user"]) ? $url["user"] : env('DB_USERNAME');
+$password = isset($url["pass"]) ? $url["pass"] : env('DB_PASSWORD');
+$database = isset($url["path"]) ? substr($url["path"], 1) : env('DB_DATABASE');
+
 use Illuminate\Support\Str;
 
 return [
@@ -46,11 +53,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', $database),
+            'username'  => env('DB_USERNAME', $username),
+            'password'  => env('DB_PASSWORD', $password),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
