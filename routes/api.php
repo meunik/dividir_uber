1a013cbbb35c7dfad436daef229d879ed9030af5
Marcos <meunik@live.com>
2022-01-07 18:42:26 -0300

    api

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', [UberController::class, 'index']);
+Route::post('/salvar', [UberController::class, 'salvar']);
+Route::get('/buscar/{data}', [UberController::class, 'buscar']);
+Route::delete('/apagar', [UberController::class, 'apagar']);
+
+Route::get('/mes/{data}', [UberController::class, 'mes']);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
