a9a524bb7d200505e56e464ac8afd94da7900c31
meunik <meunik@live.com>
2022-01-08 12:05:47 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use App\Http\Controllers\UberController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', [UberController::class, 'index']);
 Route::post('/uber/salvar', [UberController::class, 'salvar']);
